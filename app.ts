a6fe34df52b2792830994673bbf985d521f8e8e7
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-07-27 23:03:22 +0530

    setting up the projects
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as swaggerUi from 'swagger-ui-express';
 import * as fs from 'fs';
+import { APILogger } from './config/logger/api.logger';
 
 /**
  * Entry point for the application
@@ -9,4 +10,32 @@ import * as fs from 'fs';
  */
 class App {
     public express: express.Application;
+    public logger: APILogger;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+        this.logger = new APILogger()
+    }
+    
+    /**
+     * All the routes will be loaded into express
+     */
+    private routes(): void {
+        this.express.get('/', (req, res, next) => {
+            this.logger.info("All checks ok", new Date());
+            res.send("Typescript App works!!");
+        })
+    }
+    /**
+     * This method contains all the middlewares
+     * @returns void
+     */
+    private middleware(): void {
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }))
+    }
+
+
 }
---
+++ config/logger/api.logger.ts
@@ -0,0 +1,18 @@
+const pine = require('pine');
+
+const logger = pine()
+
+/**
+ * API Logger class
+ */
+export class APILogger {
+    
+    info(message: any, data: any) {
+        logger.info(`${message}   ${undefined != data ? JSON.stringify(data) : ''}`);
+    }
+
+    error(message: any) {
+        logger.error(message);
+    }
+}
+
