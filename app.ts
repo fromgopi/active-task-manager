7e96e863364e8281ed905756939ddcdd18451f0b
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-07-27 22:43:33 +0530

    setting up the projects
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ app.ts
@@ -0,0 +1,12 @@
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import * as fs from 'fs';
+
+/**
+ * Entry point for the application
+ * @param 
+ */
+class App {
+    public express: express.Application;
+}
