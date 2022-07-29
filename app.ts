10f8c77fb5a96eb5667415aeba3e86c6ba101daa
oscarmild <86421523+oscarmild@users.noreply.github.com>
2022-07-29 22:15:15 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import { APILogger } from './config/logger/api.logger';
 import { connect } from './config/db/db.config';
 import 'dotenv/config';
 
-/**
+/** 
  * Entry point for the application
  * @param  
  */ 
