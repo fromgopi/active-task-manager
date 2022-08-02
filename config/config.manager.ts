705156409d02a60a1754364e779b907807a1d064
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-08-02 21:22:31 +0530

    adding some routing stuff
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ config/config.manager.ts
@@ -0,0 +1,38 @@
+import { Config } from './config';
+import * as nconf from 'nconf'
+
+export class ConfigManager {
+    private config: Config;
+
+    constructor() {
+        this.init();
+    }
+    
+    private init() {
+        nconf.use("memory");
+
+        if (!nconf.get("info")) {
+            this.getFile();
+        }
+        this.config = nconf.get();
+        nconf.required(["port"]);
+    }
+
+    private getFile(): void {
+        nconf.env(["APP_ENV"]).file("default", {
+            file: "default.json",
+            dir: "env",
+            type: "json",
+            search: true,
+        });
+        const filename = `${process.env.APP_ENV}.json`;
+        console.log(filename);
+        nconf.file({
+            file: filename,
+            dir: "env",
+            search: true,
+            type: "json",
+        });
+    }
+
+}
