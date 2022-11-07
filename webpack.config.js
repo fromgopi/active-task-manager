0a6b08c2eaa3eb87745f32b3fb8915194d05f961
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-11-07 21:21:15 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin"); 
       
 module.exports = (env, argv) => {      
-    return {  
+    return {   
            
         entry: {app: './index.ts'}, 
 
