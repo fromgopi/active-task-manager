210513f04b107c5afa32a1d178da8b5476f98e22
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-03-06 20:03:02 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin");    
                
 
-
 module.exports = (env, argv) => {      
     return {    
              
